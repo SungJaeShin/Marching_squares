5d4c8afc68d2ed3910ca4d23f736dbc1b1bb4b6a
Sungjae Shin <67855888+SungJaeShin@users.noreply.github.com>
2024-02-07 21:21:16 +0900

    Marching Squares

--- include/parameters.h
+++ include/parameters.h
@@ -1,8 +1,12 @@
 #ifndef PARAMETERS
 #define PARAMETERS
 
+// Init Image Size
+#define WIDTH  640
+#define HEIGHT 480
+
 // For Random Grid Image
-#define DIFF_X 10
-#define DIFF_Y 10
+#define DIFF_X 50
+#define DIFF_Y 50
 
 #endif
